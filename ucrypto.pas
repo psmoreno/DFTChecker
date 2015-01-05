61304b5b5f5a549b959847ebc42e085054bda3df
Pablo Moreno <pablo.s.moreno@gmail.com>
2015-01-05 07:34:25 -0300

    improvement in ucrypto.pas to allow an AutoEncrypt system

--- ucrypto.pas
+++ ucrypto.pas
@@ -11,12 +11,14 @@ type
 
   Pcrypto=class(TObject)
     private
-
+      RAutoEncrypt:boolean;
     public
       function Encriptar(Clave:AnsiString;Cadena:AnsiString):AnsiString;
       function Desencriptar(Clave:AnsiString;Cadena:AnsiString):AnsiString;
       procedure EncriptarLog(LogOrigen:AnsiString;LogSalida:AnsiString;Clave:AnsiString);
       procedure DesencriptarLog(LogOrigen:AnsiString;LogSalida:AnsiString;Clave:AnsiString);
+      // if AutoEncrypt is true, then "Clave" is useless
+      property AutoEncrypt:boolean read RAutoEncrypt write RAutoEncrypt;
       constructor Create;
       destructor Detroy;
   end;
@@ -31,6 +33,10 @@ var
   en: TBlowFishEncryptStream;
   se: TStringStream;
 begin
+  if RAutoEncrypt then
+  begin
+    Clave:=IntToStr(FileSize(ApplicationName));
+  end;
   se := TStringStream.Create('');
   en := TBlowFishEncryptStream.Create(Trim(Clave),se);
     en.Write(Cadena[1],Length(Cadena));
@@ -45,6 +51,10 @@ var
   sd:TStringStream;
   Resultado:string;
 begin
+  if RAutoEncrypt then
+  begin
+    Clave:=IntToStr(FileSize(ApplicationName));
+  end;
   try
      sd := TStringStream.Create(DecodeStringBase64(Trim(Cadena)));
      de := TBlowFishDeCryptStream.Create(Trim(Clave),sd);
@@ -87,6 +97,7 @@ end;
 constructor Pcrypto.Create;
 begin
    inherited Create;
+   RAutoEncrypt:=false;
 end;
 
 destructor Pcrypto.Detroy;
